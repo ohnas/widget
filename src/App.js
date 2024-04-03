eb1f0fc798753c32cda79d67f949cc1a43f4f5e9
ohnas <ohnas12@gmail.com>
2024-04-03 14:38:54 +0900

    clock test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import styles from './App.module.css';
 
 function App() {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    
+    return () => {
+      clearInterval(timerId);
+    };
+  },[]);
+
   return (
-    <div>Hello</div>
+    <div>
+      <h2>현재 시간</h2>
+      <p>{time.toLocaleTimeString()}</p>
+    </div>
   );
 }
 
