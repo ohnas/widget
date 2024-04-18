0fe948d970cada953d74ae36db986ff7b05b312e
ohnas <ohnas12@gmail.com>
2024-04-18 17:32:17 +0900

    path name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Nav />
       <Routes>
-        <Route path="/" element={<Home />} ></Route>
+        <Route path="/widget" element={<Home />} ></Route>
       </Routes>
     </BrowserRouter>
   );
